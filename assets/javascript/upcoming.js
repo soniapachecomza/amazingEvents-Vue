31d1750dd35be466382b3cea25f41a6b674d5662
Sonia Pacheco <103387638+soniapachecomza@users.noreply.github.com>
2023-03-23 02:39:31 -0300

    past y upcoming en vue

--- assets/javascript/upcoming.js
+++ assets/javascript/upcoming.js
@@ -1,34 +1,38 @@
 const { createApp } = Vue
 createApp({
-
   data() {
     return {
-      date: {},
+      datos: {},
       dataEvents: [],
       checked: [],
       checkBox: [],
+      eventUpcomming: [],
       eventFiltered: [],
       textSearch: ""
+
     }
   },
   created() {
     fetch("https://mindhub-xj03.onrender.com/api/amazing")
       .then(response => response.json())
-      .then(date => {
-        this.date = date
-        this.dataEvents = this.date.events
-        this.checkBox = [... new Set(this.dataEvents.map(event => event.category))]
+      .then(datos => {
+        this.datos = datos
+        this.dataEvents = this.datos.events
+
+        this.eventUpcomming = this.dataEvents.filter(event => new Date(event.date) > new Date(this.datos.currentDate))
+        this.checkBox = [... new Set(this.eventUpcomming.map(event => event.category))]
       })
+
       .catch(error => console.log(error.message))
 
   },
   computed: {
     filteredxCheck: function filtroCheck() {
-      let eventCheck = this.checked.length == 0 ? this.dataEvents :
-        this.dataEvents.filter(evento =>
+      let eventCheck = this.checked.length == 0 ? this.eventUpcomming :
+        this.eventUpcomming.filter(evento =>
           this.checked.includes(evento.category))
       this.eventFiltered = this.textSearch == "" ? eventCheck : eventCheck.filter(event => event.name.toLowerCase().search(this.textSearch.toLowerCase().trim()) != -1)
+
     }
   }
-
 }).mount('#app')
